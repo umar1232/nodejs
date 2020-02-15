b104d7845afb74d6263f10776100cf0875a58670
Umar <umar07977@gmail.com>
2020-02-15 08:24:23 +0000

    [step1.2 not working, but moved to step 1.3]

--- node_intro/lib/messages.js
+++ node_intro/lib/messages.js
@@ -1,20 +1,35 @@
 const sanitizeHTML = require('sanitize-html');
 
+const mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost:27017/');
+
+const Message = mongoose.model(
+    'messages',
+    {username:String, text:String, _id:String}
+);
+
+//const newMessage = new Message(
+  //  {username: 'test', text: 'test2'});
+
 module.exports = function(url,callback){
-  const mongoose = require('mongoose');
+  //const mongoose = require('mongoose');
   mongoose.connect(url,callback);
 
-  const Message = mongoose.model(
+  /*const Message = mongoose.model(
     'messages',
     {}
-  );
+  );*/
 
   return {
     create:function(newMessage,callback){
-      callback();
+      //callback();
+
+        var message = new Message(newMessage);
+        message.save(callback);
     },
     read:function(id,callback){
-      callback();
+      //callback();
+        Message.findById(id,callback);
     },
     readUsername:function(username,callback){
       callback();
